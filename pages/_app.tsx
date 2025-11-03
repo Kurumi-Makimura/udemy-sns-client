791dcb7e1aaa03293e9eca7a81a0016e76d3869e
Kurumi-Makimura <kurumi1044akkle@gmail.com>
2025-11-03 17:31:02 +0900

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import Navbar from "@/src/components/Navbar";
+import { AuthProvider } from "@/src/context/auth";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <div>
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+    </AuthProvider>
+  );
+
 }
